3ece7d6f25618275852d727d6e3bff48f530038f
estein1988 <estein1988@gmail.com>
2020-11-21 17:35:31 -0700

    Change back to string.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::API
             render json: { message: 'No token' }, status: :forbidden
         else
             token = auth_header.split(' ').last
-            secret = Rails.application.secrets.secret_key_base
+            secret = 'top secret password'
             begin
                 decoded_token = JWT.decode(token, secret)
                 payload = decoded_token.first
